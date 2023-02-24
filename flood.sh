36c01660d93feb87f3ddb252d92e41f2a29f1db9
Aria Gomes <aria.gomes00@gmail.com>
2023-02-24 00:34:21 +0000

    made a mistake, fixed arm -> amd64

--- flood.sh
+++ flood.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Downloading latest flood release..."
-url=$(curl -s https://api.github.com/repos/jesec/flood/releases/latest | grep -w "browser_download_url.*flood-linux-arm64" | head -1 | cut -d : -f 2,3 | tr -d \")
+url=$(curl -s https://api.github.com/repos/jesec/flood/releases/latest | grep -w "browser_download_url.*flood-linux-x64" | head -1 | cut -d : -f 2,3 | tr -d \")
 curl -L --output "flood-ui" $url
 chmod +x ./flood-ui
 ./flood-ui -d flood/
